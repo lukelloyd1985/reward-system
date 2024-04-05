046925445276ef4f2c013550692b7af7222d95d0
Luke Lloyd <luke.lloyd1985@gmail.com>
2024-04-05 22:17:49 +0100

    added pushover to remove.php

--- remove.php
+++ remove.php
@@ -26,6 +26,15 @@
       $data->$kid->rewards = $data->$kid->rewards - 1;
       $newData = json_encode($data, JSON_PRETTY_PRINT);
       file_put_contents('kids.json', $newData);
+
+      if ($data->$kid->pushoverAppToken && $data->$kid->pushoverUserKey) {
+        $push = new Pushover();
+        $push->setToken($data->$kid->pushoverAppToken);
+        $push->setUser($data->$kid->pushoverUserKey);
+        $push->setMessage($data->$kid->name . ' removed reward');
+        $push->setUrl($_SERVER['HTTP_REFERER']);
+        $push->send();
+      }
     ?>
 
     <center><img src="<?php echo 'images/', $data->$kid->image, '/remove.gif'; ?>" height="350"></center>
