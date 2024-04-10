3a286886d22cd768b40e3f3de2d5f21fce4dde88
Luke Lloyd <92359059+lukelloyd1985@users.noreply.github.com>
2024-04-10 20:50:15 +0100

    Pushover messages include totals (#12)

--- add.php
+++ add.php
@@ -38,7 +38,7 @@
         $push = new Pushover();
         $push->setToken($data->$kid->pushoverAppToken);
         $push->setUser($data->$kid->pushoverUserKey);
-        $push->setMessage($data->$kid->name . ' added reward');
+        $push->setMessage($data->$kid->name . ' added reward' . chr(10) . chr(10) . 'cash: ' . $data->$kid->currency . $data->$kid->cash . chr(10) . 'rewards: ' . $data->$kid->rewards);
         $push->setUrl($_SERVER['HTTP_REFERER']);
         $push->send();
       }
